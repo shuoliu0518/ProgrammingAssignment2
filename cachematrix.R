09552c5826b427ca5b1cf9d634f8c64dd068ae4e
LIU SHUO <shuoliu2@gmail.com>
2016-05-08 13:17:10 +0800

    Assignment2
    Calculate the cache for the inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## Calculate the inverse matrix, if once calculated, then use the cache, if not, will do the calculation
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+inversematrix <- NULL 
+  
+  set <- function(y) 
+  {
+    m <<- y
+    inversematrix <<- NULL
+  }
+  
+  get <- function()
+  {
+    m
+  }
+  
+
+  setinverse <- function(inv) 
+  {
+    inversematrix <<- inv
+  }
+  
+ 
+  getinverse <- function() 
+  {
+    inversematrix
+  }
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)    
 
 }
 
@@ -11,5 +39,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- m$getinverse()
+  if(!is.null(inv)) 
+  {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- m$get()
+  inv <- solve(data, ...)
+  m$setinverse(inv)
+  inv    
 }
